c49c6a6864575b9dfe78e554562719d5876d55d3
Gui Macedo <98429120+macedoogui@users.noreply.github.com>
2022-04-13 17:52:30 +0000

    estrutura ok

---
+++ script.js
@@ -0,0 +1,47 @@
+const baseUrl = "http://localhost:3000/filmes";
+
+async function findAllFilmes() {
+  const response = await fetch(`${baseUrl}/find-filmes`);
+
+  const filmes = await response.json();
+};
+
+filmes.forEach((filme) => {
+    document.getElementById("banners").insertAdjacentHTML(
+      "afterbegin",
+      `<div class="FilmeLista">
+        <div>
+            <div class="FilmeLista__nome">${filme.nome}</div>
+            <div class="FilmeLista__nota">${filme.nota}</div>
+
+            <div class="FilmeLista__sinopse"> ${filme.sinopse}</div>
+          </div>
+            <img class="FilmeLista__imagem" src=${
+              filme.foto
+            } alt=${filme.nome} />
+        </div>`
+    );
+  });
+
+findAllFilmes();
+
+const findFilmeById = async () => {
+  const id = document.getElementById("idFilme").value;
+
+  const response = await fetch(`${baseUrl}/find-filme/${id}`);
+
+  const filme = await response.json();
+
+  const filmeEscolhidoDiv = document.getElementById("filmeEscolhido");
+
+  filmeEscolhidoDiv.innerHTML = `<div class="FilmeLista">
+    <div>
+      <div class="FilmeLista_nome">${filme.nome}</div>
+      <div class="FilmeLista_nota">R$ ${filme.nota.toFixed(2)}</div>
+      <div class="FilmeLista_sinopse">${filme.sinopse}</div>
+    </div>
+      <img class="FilmeLista_imagem" src=${
+        filme.imagem
+      } alt=${filme.nome}/>
+  </div>`;
+};
--- src/routes/routes.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-const FilmesController = require('../controllers/FilmesController');
-
-routes.get('/', FilmesController.getAll );
-
-
-module.export = routes;
